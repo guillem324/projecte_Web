14496af49fa4f1bd02770a8ce12d61d871b1c46d
guillem@linux <guillem.casals02@gmail.com>
2025-07-25 18:08:00 +0200

    creds

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from flask import Flask,render_template, url_for, redirect, request
 import os
 app = Flask(__name__)
 
+from google.cloud import secretmanager
+from google.oauth2 import service_account
 
-
-
+os.makedirs("secure", exist_ok=True)
+os.environ["FIREBASE_CREDENTIALS"] = "secure/firebase_service_account_info.json"
+try:
+    secret_client = secretmanager.SecretManagerServiceClient()
+    with open("secure/firebase_service_account_info.json", "w") as f:
+        f.write(secret_client.access_secret_version(request={"name": "projects/774024863265/secrets/FIREBASE_SERVICE_ACCOUNT"}).payload.data.decode("UTF-8"))
+except:
+    pass
 #inicialització db 
 import firebase_admin
 from firebase_admin import firestore, credentials
-try:
-    cred = credentials.Certificate('secure/proyecto1-56aab-198e37058f99.json')
-except:
-    cred = credentials.Certificate(os.environ["FIREBASE_SERVICE_ACCOUNT"])
+cred = credentials.Certificate(os.environ["FIREBASE_CREDENTIALS"])
 # Application Default credentials are automatically created.
 firebase_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
